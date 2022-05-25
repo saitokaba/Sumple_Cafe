c7f6c20e9eb6fe331de26411a626dbdc8b70eec1
saitokaba <103304964+saitokaba@users.noreply.github.com>
2022-05-26 03:26:00 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+$(function () {
+    $('.menu-btn').on('click', function () {        // menu-btnクラスをクリックすると、
+    $('.top-menu , .burger-menu').toggleClass('open'); // メニューとバーガーの線にopenクラスをつけ外しする
+        })
+      });
